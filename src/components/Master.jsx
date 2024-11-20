00f58a9e8f26947f3942931f60724c0a513bb322
saranya2804 <saranyachittiprolu2804@gmail.com>
2024-11-20 17:15:40 +0530

    path

--- src/components/Master.jsx
+++ src/components/Master.jsx
@@ -2,12 +2,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register';
+import DoctorDashboard from './DoctorDashboard';
 
 const Master = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/doctor" element={<DoctorDashboard/>}/>
     </Routes>
   );
 };

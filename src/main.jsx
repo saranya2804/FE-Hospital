03404588dbacb58101a78f1f2767c80d2aff0d5e
HemanthReddyEda <ehreddy14@gmail.com>
2024-11-20 12:56:08 +0530

    hem commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Master from './components/Master';
 import { BrowserRouter } from 'react-router-dom';
-
+import './App.css'
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter>
+      <Master></Master>
+  </BrowserRouter>
 );
